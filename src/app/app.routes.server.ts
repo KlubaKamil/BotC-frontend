542f496b78f55f70d364ec87ee6b7b4ef9388896
KlubaKamil <108224482+KlubaKamil@users.noreply.github.com>
2025-04-18 01:45:05 +0200

    patch v1.3
    added favicon changed rendering mode

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,23 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'games/:id',
+    renderMode: RenderMode.Client 
+  },
+  {
+    path: 'scripts/:id',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'characters/:id',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'players/:id',
+    renderMode: RenderMode.Client
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Client
   }
 ];
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  apiUrl: 'https://czachodym.ddns.net/api/'
+  apiUrl: 'https://czachodym.ddns.net/api'
 }
